0b2c02e392362e6e4bbf6c28611a5d061a7799a7
Holly Henske <hcover333@gmail.com>
2022-02-22 15:24:38 -0600

    fixed post

--- script.js
+++ script.js
@@ -16,19 +16,19 @@ window.addEventListener("load", function() {
          });    
 });
 
-fetch('https://jsonplaceholder.typicode.com/todos/', {
-    method: 'POST',
-    body: JSON.stringify({
-        userId: 201,
-        title: "wash dishes",
-        completed: false
-    }),
-    headers: {
-        "Content-type": "applications/json; charset=UTF-8"
-    }
-})
-.then(response => response.json())
-.then(json => console.log(json))
+// fetch('https://jsonplaceholder.typicode.com/todos/', {
+//     method: 'POST',
+//     body: JSON.stringify({
+//         userId: 201,
+//         title: "wash dishes",
+//         completed: false
+//     }),
+//     headers: {
+//         "Content-type": "applications/json; charset=UTF-8"
+//     }
+// })
+// .then(response => response.json())
+// .then(json => console.log(json))
 
 
 fetch('https://jsonplaceholder.typicode.com/todos/103', {
